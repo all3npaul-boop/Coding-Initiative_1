1f3490c28299ae0a8871e7ab683c22e8f2adb577
all3npaul-boop <all3n.paul@gmail.com>
2026-03-23 19:55:34 +0530

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+// Function Template
+template <class T>
+void swapValues(T &a, T &b)
+{
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    char x = 'A', y = 'B';
+
+    cout << "Before swap: a=" << a << " b=" << b << endl;
+    swapValues(a, b);
+    cout << "After swap: a=" << a << " b=" << b << endl;
+
+    cout << "\nBefore swap: x=" << x << " y=" << y << endl;
+    swapValues(x, y);
+    cout << "After swap: x=" << x << " y=" << y << endl;
+
+    return 0;
+}
