7a55392c75983c7a05939a071ee0f012b6a305ee
all3npaul-boop <all3n.paul@gmail.com>
2026-03-23 19:54:26 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+// Function Template
+template <class T>
+T findMax(T a, T b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    cout << "Max of 10 and 20 = " << findMax(10, 20) << endl;
+    cout << "Max of 4.5 and 3.2 = " << findMax(4.5, 3.2) << endl;
+
+    return 0;
+}
