2cff3654f96aca2a77a6b93752955ae866db0216
all3npaul-boop <all3n.paul@gmail.com>
2026-03-23 19:55:57 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+// Function Template with two parameters
+template <class T1, class T2>
+auto multiply(T1 a, T2 b)
+{
+    return a * b;
+}
+
+int main()
+{
+    cout << "Multiply(10, 5.5) = " << multiply(10, 5.5f) << endl;
+    cout << "Multiply(4.5, 2.2) = " << multiply(4.5f, 2.2) << endl;
+
+    return 0;
+}
