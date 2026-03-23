b6e21a035dea44a06c9de7c08652bd4612f44f2f
all3npaul-boop <all3n.paul@gmail.com>
2026-03-23 19:56:38 +0530

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+// Class Template
+template <class T>
+class Number
+{
+private:
+    T value;
+
+public:
+    Number(T v)
+    {
+        value = v;
+    }
+
+    void display()
+    {
+        cout << "Stored Value: " << value << endl;
+    }
+};
+
+int main()
+{
+    Number<int> n1(10);      // Integer object
+    Number<double> n2(5.75); // Double object
+
+    n1.display();
+    n2.display();
+
+    return 0;
+}
